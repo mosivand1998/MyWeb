f942392bda2d1848897a6e8f07d31c18a0ad4bb3
mosivand1998 <your-email@associated-with-mosivand1998.com>
2025-04-16 18:00:12 +0330

    create fist package .

---
+++ payments/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from .models import Gateway
+
+
+
+class GatewaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Gateway
+        fields = ('id','title','description','avtar')
+
+
--- utils/validators.py
+++ utils/validators.py
@@ -3,20 +3,20 @@ from django.utils.translation import gettext_lazy as _
 
 
 class PhoneNumberValidator(RegexValidator):
-    regex = '^98(9[0-3,9]\d{8}|[1-9]\d{9})$'
+    regex = r'^98(9[0-39]\d{8}|[1-9]\d{9})$'
     message = 'Phone number must be a VALID 12 digits like 98xxxxxxxxxx'
     code = 'invalid_phone_number'
 
 
 
 class SKUValidator(RegexValidator):
-    regex = '^[a-zA-Z0-9\-\]{6,20}$'
+    regex = r'^[a-zA-Z0-9-]{6,20}$'
     message = 'SKU must be alphanumeric with 6 to 20 characters'
     code = 'invalid_sku'
 
 
 class UserNameValidator(RegexValidator):
-    regex = '^[a-zA-Z][a-zA-Z0-0\,]+$'
+    regex = r'^[a-zA-Z][a-zA-Z0-9,]+$'
     message = _('Enter a valid username starting with a-z')
     code = 'invalid_username'
 
@@ -34,9 +34,9 @@ class IDNumberValidator(RegexValidator):
 
 
 class IBanNumberValidator(RegexValidator):
-    regex = '^[a-zA-Z]{2}[0-9]{2}[a-zA=Z0-9]{4}[0-9]{7}([a-zA-Z0-9]?){0,16}$'
+    regex = r'^[A-Z]{2}[0-9]{2}[A-Z0-9]{4}[0-9]{7}([A-Z0-9]?){0,16}$'
     message = _('Enter a valid iban number.')
-    code ='invalid_iban_number'
+    code = 'invalid_iban_number'
 
 class BankCardNumberValidator(RegexValidator):
     regex = '^[0-9]{16}$'
@@ -45,10 +45,10 @@ class BankCardNumberValidator(RegexValidator):
 
 
 
-validate_phone_number =PhoneNumberValidator()
+validate_phone_number = PhoneNumberValidator()
 validate_sku = SKUValidator()
 validate_username= UserNameValidator()
 validate_postal_code = PostCodeValidator()
 validate_id_number = IDNumberValidator()
 validate_iban_number = IBanNumberValidator()
-validate_bank_card_number =BankCardNumberValidator()
+validate_bank_card_number = BankCardNumberValidator()
